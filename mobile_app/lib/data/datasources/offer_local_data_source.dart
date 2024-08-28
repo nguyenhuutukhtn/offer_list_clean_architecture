acdf477c53102c6a8e31591a1ea7e6edff1b1a98
Nguyen Huu Tu <nguyenhuutukhtn>
2024-08-28 18:01:33 +0700

    add local data source

--- mobile_app/lib/data/datasources/offer_local_data_source.dart
+++ mobile_app/lib/data/datasources/offer_local_data_source.dart
@@ -1,41 +1,41 @@
-// import 'package:mobile_app/core/database/database_helper.dart';
-// import 'package:mobile_app/core/error/exceptions.dart';
-// import 'package:sqflite/sqflite.dart';
-// import '../models/offer_model.dart';
+import 'package:mobile_app/core/database/database_helper.dart';
+import 'package:mobile_app/core/error/exceptions.dart';
+import 'package:sqflite/sqflite.dart';
+import '../models/offer_model.dart';
 
-// abstract class OfferLocalDataSource {
-//   Future<List<OfferModel>> getCachedOffers();
-//   Future<void> cacheOffers(List<OfferModel> offers);
-// }
+abstract class OfferLocalDataSource {
+  Future<List<OfferModel>> getCachedOffers();
+  Future<void> cacheOffers(List<OfferModel> offers);
+}
 
-// class OfferLocalDataSourceImpl implements OfferLocalDataSource {
-//   final DatabaseHelper databaseHelper;
+class OfferLocalDataSourceImpl implements OfferLocalDataSource {
+  final DatabaseHelper databaseHelper;
 
-//   OfferLocalDataSourceImpl({required this.databaseHelper});
+  OfferLocalDataSourceImpl({required this.databaseHelper});
 
-//   @override
-//   Future<List<OfferModel>> getCachedOffers() async {
-//     try {
-//       final db = await databaseHelper.database;
-//       final maps = await db.query('offers');
-//       return List.generate(maps.length, (i) => OfferModel.fromJson(maps[i]));
-//     } catch (e) {
-//       throw CacheException();
-//     }
-//   }
+  @override
+  Future<List<OfferModel>> getCachedOffers() async {
+    try {
+      final db = await databaseHelper.database;
+      final maps = await db.query('offers');
+      return List.generate(maps.length, (i) => OfferModel.fromJson(maps[i]));
+    } catch (e) {
+      throw CacheException();
+    }
+  }
 
-//   @override
-//   Future<void> cacheOffers(List<OfferModel> offers) async {
-//     try {
-//       final db = await databaseHelper.database;
-//       await db.transaction((txn) async {
-//         await txn.delete('offers');
-//         for (var offer in offers) {
-//           await txn.insert('offers', offer.toJson());
-//         }
-//       });
-//     } catch (e) {
-//       throw CacheException();
-//     }
-//   }
-// }
+  @override
+  Future<void> cacheOffers(List<OfferModel> offers) async {
+    try {
+      final db = await databaseHelper.database;
+      await db.transaction((txn) async {
+        await txn.delete('offers');
+        for (var offer in offers) {
+          await txn.insert('offers', offer.toJson());
+        }
+      });
+    } catch (e) {
+      throw CacheException();
+    }
+  }
+}
